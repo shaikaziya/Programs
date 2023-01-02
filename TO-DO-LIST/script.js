0735548860c3071858aa6a570c6b6eab41bf0eea
shaikaziya <shaikaziya1234@gmail.com>
2023-01-02 23:48:59 +0530

    delete done

--- TO-DO-LIST/script.js
+++ TO-DO-LIST/script.js
@@ -1,64 +1,63 @@
-var toDoContainer=document.getElementById("toDoContainer")
-document.body.append(toDoContainer)
+var toDoContainer = document.getElementById("toDoContainer");
+document.body.append(toDoContainer);
 
+var ptag1 = document.getElementById("ptag");
 
-var ptag1=document.getElementById("ptag")
+var array = [];
 
-var array=[]
+var table = document.createElement("table");
+table.setAttribute("id", "table001")
 
-var table=document.createElement("table")
+var thead = document.createElement("thead");
 
+var tr1 = document.createElement("tr");
+tr1.setAttribute("id", "headerrow001");
 
-var thead=document.createElement("thead")
+var th1 = document.createElement("th");
+th1.innerText = "Name";
+var th2 = document.createElement("th");
+th2.innerText = "Delete";
+var th3 = document.createElement("th");
+th3.innerText = "Edit";
 
+var tbody = document.createElement("tbody");
+tr1.append(th1, th2, th3);
+thead.append(tr1);
 
-var tr1=document.createElement("tr")
+function add() {
+  var uiInput = document.getElementById("inputField").value;
+  //td1.innerText=ptag2
+  console.log(uiInput);
 
-var th1=document.createElement("th")
-th1.innerText="Name"
-var th2=document.createElement("th")
-th2.innerText="Delete"
-var th3=document.createElement("th")
-th3.innerText="Edit"
+  array.push(uiInput);
+  ptag1.innerText = array;
 
-var tbody=document.createElement("tbody")
+  //array.push(ptag2)
+  // toDoContainer.append(array)
+  //ptag1.append(array)
+  inputField.value = "";
 
-var tr2=document.createElement("tr")
+  var tr2 = document.createElement("tr");
+  var td1 = document.createElement("td");
+  var td2 = document.createElement("td");
+  var td3 = document.createElement("td");
 
-var td1=document.createElement("td")
-td1.innerText=""
-var td2=document.createElement("td")
-td2.innerHTML="<button onclick=Delete()>Delete</button>"
-var td3=document.createElement("td")
-td3.innerHTML="<button onclick=Edit()>Edit</button>"
-
-function Add(){
-    var ptag2=document.getElementById("inputField").value
-    td1.innerText=ptag2
-
-    array.push(ptag2)
-    // toDoContainer.append(array)
-    ptag1.append(array)
-    inputField.value=""
-    
-    
+  td1.innerText = uiInput;
+  td2.innerHTML = `<button onclick=Delete('${td1.innerText}')>Delete</button>`
+  td3.innerHTML = "<button onclick=Edit()>Edit</button>";
+  tr2.setAttribute("id", td1.innerText);
+  tr2.append(td1, td2, td3);  
+  tbody.append(tr2);
+  
 }
 
-function Delete(){
-    // toDoContainer.removeChild(toDoContainer.firstElementChild);
-    td1.remove();
-    // ptag1.removeChild(ptag1.firstElementChild);
-
-
-
+function Delete(param1) {
+   console.log("acutalattributeId  " + param1);
+    console.log(table.getAttribute("id"));
+    document.getElementById(param1).remove();
 }
 
-function Edit(){
-}
+function Edit() {}
 
-tr2.append(td1,td2,td3)
-tr1.append(th1,th2,th3)
-tbody.append(tr2)
-thead.append(tr1)
-table.append(tbody,thead)
-document.body.append(table)
+table.append(tbody, thead);
+document.body.append(table);
